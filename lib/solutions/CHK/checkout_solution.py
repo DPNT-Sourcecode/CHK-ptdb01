50c6523108d288246d5a77d07ec313780b6eaba0
sbx_user1051 <sbx_user1051@169.254.64.53>
2024-09-26 22:59:32 +0000

    Thu Sep 26 22:59:32 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,36 +9,32 @@ def checkout(skus):
     - Check there are no characters besides A, B, C or D in the string.
     """
     
-    if (type(skus) == str) and all([character in ['A','B','C','D','E','F'] for character in skus]):
-        items = {
-            'A': 0,
-            'B': 0,
-            'C': 0,
-            'D': 0,
-            'E': 0,
-            'F': 0
-        }
+    if (type(skus) == str) and all([character in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for character in skus]):
+
+        items = {character: 0 for character in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
         
         for character in skus:
             if character in items:
                 items[character] += 1
         
-        costs = {
-            'A': 0,
-            'B': 0,
-            'C': 0,
-            'D': 0,
-            'E': 0,
-            'F': 0
-        }
+        costs = {character: 0 for character in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
         
-        B_to_buy = max([items['B'] - (items['E']//2), 0])
+        non_offer_coeffs = {'C': 20, 'D': 15, 'E': 40, 'G': 20, 'I': 35, 'J': 60, 'L': 90, 'N': 15, 'O': 10, 'R': 50, 'S': 30, 'T': 20, 'W': 20, 'X': 90, 'Y': 10, 'Z': 50}
+        for non_offer_char in 'CDGIJLMOSTWXYZ':
+            costs[non_offer_char] = non_offer_coeffs[non_offer_char]*items[non_offer_char]
+        
         costs['A'] = 200*(items['A']//5) + 130*((items['A'] % 5)//3) + 50*((items['A'] % 5) % 3)
+        B_to_buy = max([items['B'] - (items['E']//2), 0])
         costs['B'] = 45*(B_to_buy//2) + 30*(B_to_buy % 2)
-        costs['C'] = 20*items['C']
-        costs['D'] = 15*items['D']
-        costs['E'] = 40*items['E']
         costs['F'] = 20*(items['F']//3) + 10*(items['F'] % 3)
+        costs['H'] = 80*(items['H']//10) + 45*((items['H'] % 10)//5) + 10*((items['H'] % 10) % 5)
+        costs['K'] = 150*(items['K']//2) + 80*(items['K'] % 2)
+        costs['M'] = 20*max([items['M'] - (items['N']//3), 0])
+        costs['P'] = 200*(items['P']//5) + 50*(items['P'] % 5)
+        Q_to_buy = max([items['Q'] - (items['R']//3), 0])
+        costs['Q'] = 80*(Q_to_buy//3) + 30*(Q_to_buy % 3)
+        costs['U'] = 80*(items['U']//3) + 40*(items['U'] % 3)
+        costs['V'] = 130*(items['V']//3) + 90*((items['V'] % 3)//2) + 50*((items['V'] % 3) % 2)
         
         total = sum(list(costs.values()))
         
