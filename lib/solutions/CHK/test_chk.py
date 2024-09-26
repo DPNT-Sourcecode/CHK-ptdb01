77fbd50b497bcd040c011cd530c367ac4e3c41a2
sbx_user1051 <sbx_user1051@169.254.64.53>
2024-09-26 19:59:06 +0000

    Thu Sep 26 19:59:06 UTC 2024

--- lib/solutions/CHK/test_chk.py
+++ lib/solutions/CHK/test_chk.py
@@ -1,13 +1,5 @@
 import checkout_solution as sol
-
-print(sol.checkout("2ABCBABCBABCBAC2222"))
-print(sol.checkout("A, B, C, D, E, A, B, C"))
-
-# +------+-------+----------------+
-# | Item | Price | Special offers |
-# +------+-------+----------------+
-# | A    | 50    | 3A for 130     |
-# | B    | 30    | 2B for 45      |
-# | C    | 20    |                |
-# | D    | 15    |                |
-# +------+-------+----------------+
+print(sol.checkout("2ABCBABCBABCBAC2222")==395)
+print(sol.checkout("A, B, C, D, E, A, B, C")==200)
+print(sol.checkout(420)==-1)
+print(sol.checkout(["A","B","C","D"])==-1)
