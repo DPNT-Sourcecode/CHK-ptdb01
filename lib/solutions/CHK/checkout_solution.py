f4747b78e28dab627004b4222ea894a343861dd9
sbx_user1051 <sbx_user1051@169.254.64.53>
2024-09-26 22:27:23 +0000

    Thu Sep 26 22:27:23 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,27 +9,38 @@ def checkout(skus):
     - Check there are no characters besides A, B, C or D in the string.
     """
     
-    if (type(skus) == str) and all([character in ['A','B','C','D','E'] for character in skus]):
-        order = {
+    if (type(skus) == str) and all([character in ['A','B','C','D','E','F'] for character in skus]):
+        items = {
             'A': 0,
             'B': 0,
             'C': 0,
             'D': 0,
-            'E': 0
+            'E': 0,
+            'F': 0
         }
         
         for character in skus:
-            if character in order:
-                order[character] += 1
+            if character in items:
+                items[character] += 1
         
-        B_to_buy = max([order['B'] - (order['E']//2), 0])
-        A_cost = 200*(order['A']//5) + 130*((order['A'] % 5)//3) + 50*((order['A'] % 5) % 3)
-        B_cost = 45*(B_to_buy//2) + 30*(B_to_buy % 2)
-        C_cost = 20*order['C']
-        D_cost = 15*order['D']
-        E_cost = 40*order['E']
+        costs = {
+            'A': 0,
+            'B': 0,
+            'C': 0,
+            'D': 0,
+            'E': 0,
+            'F': 0
+        }
         
-        total = A_cost + B_cost + C_cost + D_cost + E_cost
+        B_to_buy = max([items['B'] - (items['E']//2), 0])
+        costs['A'] = 200*(items['A']//5) + 130*((items['A'] % 5)//3) + 50*((items['A'] % 5) % 3)
+        costs['B'] = 45*(B_to_buy//2) + 30*(B_to_buy % 2)
+        costs['C'] = 20*items['C']
+        costs['D'] = 15*items['D']
+        costs['E'] = 40*items['E']
+        costs['F'] = 20*(items['F']//3) + 10*(items['F'] % 3)
+        
+        total = sum(list(costs.values()))
         
         return int(total)
     
