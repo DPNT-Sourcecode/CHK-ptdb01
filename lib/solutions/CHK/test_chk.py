b834852489cd0afe3c4d56264d8ce51b864bc406
sbx_user1051 <sbx_user1051@169.254.64.53>
2024-09-26 20:02:07 +0000

    Thu Sep 26 20:02:07 UTC 2024

--- lib/solutions/CHK/test_chk.py
+++ lib/solutions/CHK/test_chk.py
@@ -3,3 +3,5 @@ print(sol.checkout("2ABCBABCBABCBAC2222")==395)
 print(sol.checkout("A, B, C, D, E, A, B, C")==200)
 print(sol.checkout(420)==-1)
 print(sol.checkout(["A","B","C","D"])==-1)
+
+print(type(['a']) == str)
