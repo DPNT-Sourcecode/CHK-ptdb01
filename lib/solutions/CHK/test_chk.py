87727e82baff3672c9c77a0481b55c2cd4c4aaf3
sbx_user1051 <sbx_user1051@169.254.64.53>
2024-09-26 20:37:30 +0000

    Thu Sep 26 20:37:30 UTC 2024

--- lib/solutions/CHK/test_chk.py
+++ lib/solutions/CHK/test_chk.py
@@ -16,6 +16,7 @@ print(sol.checkout("ABCa") == -1)
 print(sol.checkout("AxA") == -1)
 print(sol.checkout("ABCD") == 115)
 print("A",sol.checkout("A") == 50)
+print(200*(1//5) + 130*((1 % 5)//3) + 50*((1 % 5) % 3))
 print("AA",sol.checkout("AA") == 100)
 print("AAA",sol.checkout("AAA") == 130)
 print("AAAA",sol.checkout("AAAA") == 180)
@@ -30,7 +31,7 @@ print("BABDDCAC",sol.checkout("BABDDCAC") == 215)
 print("AAABB",sol.checkout("AAABB") == 175)
 print("ABCDCBAABCABBAAA",sol.checkout("ABCDCBAABCABBAAA") == 495)
 
-print("ABCDCBAABCABBAAAEEE",sol.checkout("ABCDCBAABCABBAAAEEE") == 615)
+print("ABCDCBAABCABBAAAEEE",sol.checkout("ABCDCBAABCABBAAAEEE") == 585)
 print("ABCDCBAABCABBAAAE",sol.checkout("ABCDCBAABCABBAAAE") == 535)
 print("BBBB",sol.checkout("BBBB") == 90)
 print("BBBBEEEEEEEEEE",sol.checkout("BBBBEEEEEEEEEE") == 240)
@@ -44,4 +45,5 @@ print("BBBBEEEEEEEEEE",sol.checkout("BBBBEEEEEEEEEE") == 240)
 # | D    | 15    |                        |
 # | E    | 40    | 2E get one B free      |
 # +------+-------+------------------------+
+
 
