25a0997bf02c26f3d8b63b76ffe338c4f8eb76e8
sbx_user1051 <sbx_user1051@169.254.64.53>
2024-09-26 19:46:12 +0000

    Thu Sep 26 19:46:12 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,24 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    """
+    Input string is not defined. we'll intentonially fail to find out.
+    """
+
+    order = {
+        'A': 0,
+        'B': 0,
+        'C': 0,
+        'D': 0
+    }
+    
+    for 
+    
+    
     raise NotImplementedError()
+
+
+A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
