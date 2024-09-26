93a7c65f0f61cd9d3d8aae82b0f071da72f42bc8
sbx_user1051 <sbx_user1051@169.254.64.53>
2024-09-26 19:55:14 +0000

    Thu Sep 26 19:55:14 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,18 +6,24 @@ def checkout(skus):
     """
     Input string is not defined. ignore characters that aren't A, B, C or D.
     """
+    
+    try:
+        
+        order = {
+            'A': 0,
+            'B': 0,
+            'C': 0,
+            'D': 0
+        }
+        
+        for character in skus:
+            if character in order:
+                order[character] += 1
+        
+        total = 130*(order['A']//3) + 50*(order['A'] % 3) + 45*(order['B']//2) + 30*(order['B'] % 2) + 20*order['C'] + 15*order['D']
+        
+        return int(total)
+    
+    except:
+        return -1
 
-    order = {
-        'A': 0,
-        'B': 0,
-        'C': 0,
-        'D': 0
-    }
-    
-    for character in skus:
-        if character in order:
-            order[character] += 1
-    
-    total = 130*(order['A']//3) + 50*(order['A'] % 3) + 45*(order['B']//2) + 30*(order['B'] % 2) + 20*order['C'] + 15*order['D']
-    
-    return total
